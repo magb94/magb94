854ff136517c7c4b5eb76d32c470105c9389f2ac
magb94 <magb94pc@gmail.com>
2017-03-17 16:30:59 +0100

    dossier Controller

--- README 2.md
+++
@@ -1,4 +0,0 @@
-magb94
-======
-
-A Symfony project created on March 15, 2017, 11:47 am.
--- src/MagbBundle/MagbBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace MagbBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MagbBundle extends Bundle
-{
-}
